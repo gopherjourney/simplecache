91fe6f8d600acbe3ee6ee3e384dbb2c0b02bf992
Arham Abiyan <arham.abiyan@gmail.com>
2023-09-10 09:26:50 +0700

    remove expiration to simplify it at first

--- simple_cache.go
+++ simple_cache.go
@@ -2,27 +2,12 @@ package simplecache
 
 import (
 	"sync"
-	"time"
-)
-
-const (
-	NoExpiration = -1
 )
 
 type Entry struct {
 	key string
 
 	value interface{}
-
-	expiration int64
-}
-
-func (e *Entry) Expired() bool {
-	if e.expiration > 0 {
-		return time.Now().UnixNano() > e.expiration
-	}
-
-	return false
 }
 
 type Cache struct {
@@ -39,33 +24,18 @@ func New() *Cache {
 }
 
 func (c *Cache) Set(key string, value interface{}) {
-	c.SetWithTTL(key, value, NoExpiration)
-}
-
-func (c *Cache) SetWithTTL(key string, value interface{}, ttl time.Duration) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 
-	exp := time.Now().Add(ttl).UnixNano()
-	if ttl == NoExpiration {
-		exp = NoExpiration
-	}
-
 	c.entries[key] = &Entry{
-		key:        key,
-		value:      value,
-		expiration: exp,
+		key:   key,
+		value: value,
 	}
 }
 
 func (c *Cache) Get(key string) (interface{}, bool) {
 	v, ok := c.entries[key]
 	if !ok {
-		return nil, false
-	}
-
-	if v.Expired() {
-		c.Delete(key)
 		return nil, false
 	}
 
